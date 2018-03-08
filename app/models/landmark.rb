7bf162cb9bfaa9109eb3203000085daae88f54e6
An Nuyen <annuyen@gmail.com>
2018-03-08 01:58:10 +0000

    automatically backed up by learn

--- app/models/landmark.rb
+++ app/models/landmark.rb
@@ -1,4 +1,4 @@
-class Landmark <ActiveRecord::Base
-  belongs_to :figure 
-  has_many :titles, through: :figures 
+class Landmark < ActiveRecord::Base
+  belongs_to :figure
+  has_many :titles, through: :figures
 end
