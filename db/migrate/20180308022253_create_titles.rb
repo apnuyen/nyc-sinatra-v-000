e7c29f921e6395ece2d2710ecc08f1e89096115a
An Nuyen <annuyen@gmail.com>
2018-03-08 02:23:29 +0000

    automatically backed up by learn

--- db/migrate/20180308022253_create_titles.rb
+++ db/migrate/20180308022253_create_titles.rb
@@ -1,7 +1,7 @@
 class CreateTitles < ActiveRecord::Migration[5.1]
   def change
-    create_table :titles do |t| 
-      t.string :name 
-    end 
+    create_table :titles do |t|
+      t.string :name
+    end
   end
 end
