222fd6eb3f23b0af72507bb73f6787835b4514f4
An Nuyen <annuyen@gmail.com>
2018-03-08 01:52:57 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,24 +1,39 @@
-class FiguresController < ApplicationController 
+class FiguresController < ApplicationController
 
-  get "/figures" do 
-    @figures = Figure.all 
+  get "/figures" do
+    @figures = Figure.all
     erb :"figures/index"
-  end 
+  end
 
-  get "/figures/new" do 
+  get "/figures/new" do
     erb :"figures/new"
-  end 
+  end
 
-  post "/figures" do 
+  post "/figures" do
     figure = Figure.new(params[:figure])
     figure.landmarks.find_or_initialize_by(params[:landmark])
     figure.titles.find_or_initialize_by(params[:title])
-    figure.save 
+    figure.save
     redirect to "/figures/#{figure.id}"
+  end
+
+  get "/figures/:id" do
+    @figure = Figure.find(params[:id])
+    erb :"figures/show"
+  end
+
+  get "/figures/:id/edit" do 
+    @figure = Figure.find(params[:id])
+    erb :"figures/edit" 
   end 
 
-  get "/figures/:id" do 
-    @figure = Figure.find(params[:id])
-    erb :"figures/show" 
+  patch "/figures/:id" do 
+    figure = Figure.find(params[:id])
+    figure.update(params[:figure])
+    figure.landmarks.find_or_create_by(params[:landmark])
+    figure.titles.find_or_create_by(params[:title])
+
+    redirect to "/figures/#{figure.id}"
   end 
-end 
+end
+
