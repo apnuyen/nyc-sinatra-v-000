3bf7defd0b0df398bc842072136f24af991d5c58
An Nuyen <annuyen@gmail.com>
2018-03-08 01:56:06 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,31 +1,31 @@
 class LandmarksController < ApplicationController
-  get "landmarks" do 
-    @landmarks = Landmark.all 
-    erb :"landmarks/index" 
-  end 
+  get "landmarks" do
+    @landmarks = Landmark.all
+    erb :"landmarks/index"
+  end
 
-  get "/landmarks/new" do 
-    erb :"landmarks/new" 
-  end 
+  get "/landmarks/new" do
+    erb :"landmarks/new"
+  end
 
-  post "/landmarks" do 
+  post "/landmarks" do
     landmark = Landmark.create(params[:landmark])
     redirect to "/landmarks/#{landmark.id}"
-  end 
+  end
 
-  patch "/landmarks/:id" do 
+  patch "/landmarks/:id" do
     landmark = Landmark.find(params[:id])
     landmark.update(params[:landmark])
     redirect to "/landmarks/#{landmark.id}"
-  end 
+  end
 
-  get "/landmarks/:id/edit" do 
+  get "/landmarks/:id/edit" do
     @landmark = Landmark.find(params[:id])
     erb :"/landmarks/edit"
-  end 
+  end
 
-  get "/landmarks/:id" do 
+  get "/landmarks/:id" do
     @landmark = Landmark.find(params[:id])
-    erb :"landmarks/show" 
-  end 
+    erb :"landmarks/show"
+  end
 end
